3187a2a250260fa78dc78dcce3426f851c75d98b
RomanBorr <starmordar@gmail.com>
2019-10-24 22:31:29 +0300

    add name selection

--- entryPoint.sh
+++ entryPoint.sh
@@ -8,22 +8,31 @@
 # echo ''
 
 IP=$(
-    curl -X GET \
+    curl --silent -X GET \
         -H "Content-type: application/json" \
         -H "Accept: application/json" \
         -d '{"name": "Starmordar", "password": "Msims7529"}' \
         localhost:4000/getWorkspacesCurl
 )
 
-mapfile -t lines <<< $(jq '.[].category' <<<  $IP)
+mapfile -t lines <<<$(jq '.[].category' <<<$IP)
 
-echo ${lines}
-
-IFS=' ' read -ra my_array <<< "$lines"
+IFS=' ' read -ra my_array <<<"$lines"
 
 sorted_unique_ids=($(echo "${lines[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
-for i in "${sorted_unique_ids[@]}"
-do
-    echo $i
+echo "Input on of the available workspaces: "
+for i in "${sorted_unique_ids[@]}"; do
+    echo "${i//\"/}"
+done
+
+read category
+
+for i in "${sorted_unique_ids[@]}"; do
+    if [ "${i//\"/}" = "$category" ]; then
+
+        mapfile -t lines2 <<<$(jq -r '.[] | select(.category=="School") | .name' <<<$IP)
+        echo $lines2
+
+    fi
 done
