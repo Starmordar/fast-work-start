b8536e50e9b97a8a3ef3e03de9d37aa17deb5d29
RomanBorr <starmordar@gmail.com>
2019-10-24 21:35:32 +0300

    select unique categories from JSON

--- entryPoint.sh
+++ entryPoint.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-echo -n "Username for 'http://example.com/': "
-read username
+# echo -n "Username for 'http://example.com/': "
+# read username
 
-echo -n "Password for 'http://${username}@example.com/': "
-read -s password
-echo ''
+# echo -n "Password for 'http://${username}@example.com/': "
+# read -s password
+# echo ''
 
+IP=$(
+    curl -X GET \
+        -H "Content-type: application/json" \
+        -H "Accept: application/json" \
+        -d '{"name": "Starmordar", "password": "Msims7529"}' \
+        localhost:4000/getWorkspacesCurl
+)
 
-IP=$(curl -X GET -H "Content-type: application/json" -H "Accept: application/json" -d '{"name": "'${username}'", "password": "'${password}'"}' localhost:4000/getWorkspacesCurl)
+mapfile -t lines <<< $(jq '.[].category' <<<  $IP)
 
-echo $IP
+echo ${lines}
+
+IFS=' ' read -ra my_array <<< "$lines"
+
+sorted_unique_ids=($(echo "${lines[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+for i in "${sorted_unique_ids[@]}"
+do
+    echo $i
+done
